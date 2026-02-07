157b4817061a923478810761a9b933d866ac9140
Inceptify <inceptify1@gmail.com>
2026-02-07 17:17:00 -0500

    Setup Note Timing Zones
    - Created Bad, Okay, Good & Perfect timing zones for notes
    > Must add functionality with timing notes with specific keybinds

--- Assets/Streaming/RythmGame/Scripts/NoteMovement.cs
+++ Assets/Streaming/RythmGame/Scripts/NoteMovement.cs
@@ -3,7 +3,15 @@ using UnityEngine;
 public class NoteMovement : MonoBehaviour
 {
     [SerializeField] float noteSpeed = 3f;
+    string[] zone = new string[4];
 
+    private void Start()
+    {
+        zone[0] = "Bad";
+        zone[1] = "Okay";
+        zone[2] = "Good";
+        zone[3] = "Perfect";
+    }
     private void Update()
     {
         float yPos = transform.position.y;
@@ -14,5 +22,26 @@ public class NoteMovement : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //JUST TESTING WITH 'W' KEY
+        //MUST SET UP TO WORK WITH THEIR RESPECTIVE LANES
+        //MUST SET A VARIABLE TO SEE WHAT ZONE THE PLAYER IS IN TO HANDLE INPUT IN FIXED UPDATE
+
+        //if (collision.tag == zone[0])
+        //    {
+        //        Debug.Log("HIT BAD");
+        //    } else if (collision.tag == zone[2])
+        //    {
+        //        Debug.Log("HIT OKAY");
+        //    } else if (collision.tag == zone[3])
+        //    {
+        //        Debug.Log("HIT GOOD");
+        //    } else
+        //    {
+        //        Debug.Log("HIT PERFECT");
+        //    }
     }
 }
