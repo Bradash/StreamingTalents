b6ff5c3439effc65ae6ccd829a9519deb0cd9a5a
Inceptify <inceptify1@gmail.com>
2026-02-08 18:17:18 -0500

    Changed class name for tycoon game state

--- Assets/Streaming/Tycoon/Scripts/TycoonGameState.cs
+++ Assets/Streaming/Tycoon/Scripts/TycoonGameState.cs
@@ -1,9 +1,8 @@
 using UnityEngine;
 
-public class GameState : MonoBehaviour
+public class TycoonGameState : MonoBehaviour
 {
     [SerializeField] GameObject exitUI;
-
     public void EnableUI(GameObject enable)
     {
         enable.SetActive(true);
