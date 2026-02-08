c7934c53728b02745f41f353bc788cb3ef6f96d3
Inceptify <inceptify1@gmail.com>
2026-02-08 17:36:15 -0500

    Lane Changer mini-game prototype ready
    - Created game over screen allowing player to retry or quit on death
    - Added game reset functionality

--- Assets/Streaming/LaneChanger/Scripts/LanePlayer.cs
+++ Assets/Streaming/LaneChanger/Scripts/LanePlayer.cs
@@ -1,48 +1,73 @@
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 public class LanePlayer : MonoBehaviour
 {
+    Vector3 initialPos;
     [SerializeField] Vector3[] lanePositions;
     Vector3 targetLane;
     int currentLane = 1;
-
+    bool playerAlive = true;
+    [SerializeField] GameObject gameOver;
     [SerializeField] float moveSpeed = 5f;
 
     void Start()
     {
+        gameOver.SetActive(false);
+        initialPos = transform.position;
         targetLane = lanePositions[currentLane];
         transform.position = lanePositions[currentLane];
+
+        LaneSpawner.resetGameState += ResetState;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        if (playerAlive)
         {
-            currentLane = Mathf.Clamp(currentLane + 1, 0, 2);
-            targetLane = lanePositions[currentLane];
-        }
-        else if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
-        {
-            currentLane = Mathf.Clamp(currentLane - 1, 0, 2);
-            targetLane = lanePositions[currentLane];
-        }
+            if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            {
+                currentLane = Mathf.Clamp(currentLane + 1, 0, 2);
+                targetLane = lanePositions[currentLane];
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            {
+                currentLane = Mathf.Clamp(currentLane - 1, 0, 2);
+                targetLane = lanePositions[currentLane];
+            }
 
-        Movement();
+            Movement();
+        }
     }
 
     void Movement()
     {
-        Vector3 pos = transform.position;
-        if (Mathf.Abs(targetLane.x - pos.x) > 0.1f) {
-            float direction = Mathf.Sign(targetLane.x - pos.x);
-            pos.x += moveSpeed * Time.deltaTime * direction;
-            transform.position = pos;
+        if (playerAlive)
+        {
+            Vector3 pos = transform.position;
+            if (Mathf.Abs(targetLane.x - pos.x) > 0.1f)
+            {
+                float direction = Mathf.Sign(targetLane.x - pos.x);
+                pos.x += moveSpeed * Time.deltaTime * direction;
+                transform.position = pos;
+            }
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
+        playerAlive = false;
+        Vector3 pos = transform.position;
+        pos.y -= 100;
+        transform.position = pos;
+        gameOver.SetActive(true);
+    }
+
+   void ResetState()
+    {
+        gameOver.SetActive(false);
+        transform.position = initialPos;
+        currentLane = 1;
+        targetLane = lanePositions[currentLane];
+        playerAlive = true;
     }
 }
--- Assets/Streaming/LaneChanger/Scripts/LaneSpawner.cs
+++ Assets/Streaming/LaneChanger/Scripts/LaneSpawner.cs
@@ -2,11 +2,21 @@ using UnityEngine;
 
 public class LaneSpawner : MonoBehaviour
 {
-    [SerializeField] float spawnTimer = 5f;
+    [SerializeField] float spawnTimer = 3f;
     float currentTime = 0f;
+
+    public float startTime;
 
     [SerializeField] GameObject obstacle;
     [SerializeField] GameObject[] spawns;
+
+    public delegate void ResetGameState();
+    public static event ResetGameState resetGameState;
+
+    private void Start()
+    {
+        startTime = Time.time;
+    }
 
     void Update()
     {
@@ -14,13 +24,52 @@ public class LaneSpawner : MonoBehaviour
 
         if (currentTime < 0)
         {
-            int randomSpawn = Random.Range(0, spawns.Length);
-            Instantiate(obstacle, spawns[randomSpawn].transform.position, Quaternion.identity);
+            int randomSpawn = Random.Range(1, 7);
+            Debug.Log(randomSpawn);
+
+            switch(randomSpawn)
+            {
+                case 1:
+                    Instantiate(obstacle, spawns[0].transform.position, Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(obstacle, spawns[1].transform.position, Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(obstacle, spawns[2].transform.position, Quaternion.identity);
+                    break;
+                case 4:
+                    Instantiate(obstacle, spawns[0].transform.position, Quaternion.identity);
+                    Instantiate(obstacle, spawns[1].transform.position, Quaternion.identity);
+                    break;
+                case 5:
+                    Instantiate(obstacle, spawns[0].transform.position, Quaternion.identity);
+                    Instantiate(obstacle, spawns[2].transform.position, Quaternion.identity);
+                    break;
+                case 6:
+                    Instantiate(obstacle, spawns[1].transform.position, Quaternion.identity);
+                    Instantiate(obstacle, spawns[2].transform.position, Quaternion.identity);
+                    break;
+            }
+
             currentTime = spawnTimer;
-            if(spawnTimer > 1)
+            if (spawnTimer > 1)
             {
                 spawnTimer -= 0.25f;
             }
         }
+    }
+
+    public float GetStartTime()
+    {
+        return startTime;
+    }
+
+    public void ResetGame()
+    {
+        resetGameState?.Invoke();
+        startTime = Time.time;
+        spawnTimer = 3f;
+        currentTime = 0f;
     }
 }
