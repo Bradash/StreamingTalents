fb4f1be9cd8d2faa099c8429d8261ccc9c3ea53c
Mac Mealey <148462018+Maccy-0@users.noreply.github.com>
2026-02-22 21:35:42 -0500

    Changed 1 varable

--- Assets/Streaming/SceneLoader.cs
+++ Assets/Streaming/SceneLoader.cs
@@ -5,7 +5,7 @@ public class SceneLoader : MonoBehaviour
 {
     public void LoadScene(string sceneName)
     {
-        if (sceneName == "MainGame")
+        if (sceneName == "IRL")
         {
             if (GameManager.currentday == 3)
             {
