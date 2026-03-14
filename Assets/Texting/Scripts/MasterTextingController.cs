6dd2d4526c765ee9bb91b933a07e71ede6c6b49b
Mac Mealey <148462018+Maccy-0@users.noreply.github.com>
2026-03-13 23:06:48 -0400

    Added text navagation

--- Assets/Texting/Scripts/MasterTextingController.cs
+++ Assets/Texting/Scripts/MasterTextingController.cs
@@ -1,7 +1,21 @@
+using TMPro;
 using UnityEngine;
 
 public class MasterTextingController : MonoBehaviour
 {
+
+    public GameObject quitbutton;
+    public GameObject newsquitbutton;
+    public GameObject newsbutton;
+    public GameObject textoptions;
+    public GameObject backarrow;
+    public GameObject profile;
+    public GameObject startingmessagearea;
+    public GameObject messagearea;
+    public GameObject newsarea;
+
+    public TextMeshProUGUI Title;
+
     public static MasterTextingController Instance { get; private set; }
 
     void Awake()
@@ -17,18 +31,58 @@ public class MasterTextingController : MonoBehaviour
         showMain();
     }
 
-    void showMain()
+    public void showMain()
     {
+        //Change screeen
+
+        quitbutton.SetActive(true);
+        newsbutton.SetActive(true);
+        newsquitbutton.SetActive(false);
+        textoptions.SetActive(false);
+        backarrow.SetActive(false);
+        profile.SetActive(false);
+        startingmessagearea.SetActive(true);
+        messagearea.SetActive(false);
+        newsarea.SetActive(false);
+
+        Title.text = "Messages";
+
 
     }
 
-    void showMessage()
+    public void showMessage(MessageGroup character)
     {
+        //Change screeen
+
+        quitbutton.SetActive(false);
+        newsbutton.SetActive(false);
+        newsquitbutton.SetActive(false);
+        textoptions.SetActive(true);
+        backarrow.SetActive(true);
+        profile.SetActive(true);
+        startingmessagearea.SetActive(false);
+        messagearea.SetActive(true);
+        newsarea.SetActive(false);
+
+        Title.text = character.displayName;
 
     }
 
-    void showNews()
+    public void showNews()
     {
+        //Change screeen
+
+        quitbutton.SetActive(false);
+        newsbutton.SetActive(false);
+        newsquitbutton.SetActive(true);
+        textoptions.SetActive(false);
+        backarrow.SetActive(false);
+        profile.SetActive(false);
+        startingmessagearea.SetActive(false);
+        messagearea.SetActive(false);
+        newsarea.SetActive(true);
+
+        Title.text = "News";
 
     }
 
