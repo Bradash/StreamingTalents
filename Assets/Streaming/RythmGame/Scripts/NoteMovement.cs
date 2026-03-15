8bde56ce47b05d795cf918687e1e3c4446254c0e
Inceptify <inceptify1@gmail.com>
2026-03-15 19:28:56 -0400

    Finished Notes mechanics for Rythm Game
    - Player can time notes with key click
    - Notes are locked to specific key clicks by lanes
    - Notes disappear if timed correctly

--- Assets/Streaming/RythmGame/Scripts/NoteMovement.cs
+++ Assets/Streaming/RythmGame/Scripts/NoteMovement.cs
@@ -3,49 +3,16 @@ using UnityEngine;
 public class NoteMovement : MonoBehaviour
 {
     [SerializeField] float noteSpeed = 3f;
-    [SerializeField] int noteLane;
-    string[] zone = new string[4];
-    string currentZone = "Miss";
 
-    private void Start()
-    {
-        zone[0] = "Bad";
-        zone[1] = "Okay";
-        zone[2] = "Good";
-        zone[3] = "Perfect";
-    }
     private void Update()
     {
         float yPos = transform.position.y;
         yPos -= noteSpeed * Time.deltaTime;
         transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
 
-        if(yPos < -6)
+        if (yPos < -6)
         {
             Destroy(gameObject);
         }
-
-
-    }
-
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        //JUST TESTING WITH 'W' KEY
-        //MUST SET UP TO WORK WITH THEIR RESPECTIVE LANES
-        //MUST SET A VARIABLE TO SEE WHAT ZONE THE PLAYER IS IN TO HANDLE INPUT IN FIXED UPDATE
-
-        //if (collision.tag == zone[0])
-        //    {
-        //        Debug.Log("HIT BAD");
-        //    } else if (collision.tag == zone[2])
-        //    {
-        //        Debug.Log("HIT OKAY");
-        //    } else if (collision.tag == zone[3])
-        //    {
-        //        Debug.Log("HIT GOOD");
-        //    } else
-        //    {
-        //        Debug.Log("HIT PERFECT");
-        //    }
     }
 }
---
+++ Assets/Streaming/RythmGame/Scripts/NoteTiming.cs
@@ -0,0 +1,119 @@
+using UnityEngine;
+
+public class NoteTiming : MonoBehaviour
+{
+    [SerializeField] int noteLane;
+    string[] zone = new string[5];
+    string currentZone;
+    [SerializeField] SpriteRenderer noteColor;
+
+    private void Start()
+    {
+        zone[0] = "Miss";
+        zone[1] = "Bad";
+        zone[2] = "Okay";
+        zone[3] = "Good";
+        zone[4] = "Perfect";
+
+        currentZone = zone[0];
+
+        switch (noteLane)
+        {
+            case 0:
+                noteColor.color = new Color32(130, 207, 255, 255);
+                break;
+            case 1:
+                noteColor.color = new Color32(255, 127, 227, 255);
+                break;
+            case 2:
+                noteColor.color = new Color32(255, 177, 128, 255);
+                break;
+            case 3:
+                noteColor.color = new Color32(155, 255, 125, 255);
+                break;
+        }
+    }
+
+    private void Update()
+    {
+        switch (noteLane)
+        {
+            case 0:
+                if(Input.GetKeyDown(KeyCode.W)) {
+                    HitZone();
+                }
+                break;
+            case 1:
+                if (Input.GetKeyDown(KeyCode.A))
+                {
+                    HitZone();
+                }
+                break;
+            case 2:
+                if (Input.GetKeyDown(KeyCode.S))
+                {
+                    HitZone();
+                }
+                break;
+            case 3:
+                if (Input.GetKeyDown(KeyCode.D))
+                {
+                    HitZone();
+                }
+                break;
+        }
+
+        if (transform.position.y < -4.7)
+        {
+            currentZone = zone[0];
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == zone[1])
+        {
+            currentZone = zone[1];
+        }
+        else if (collision.tag == zone[2])
+        {
+            currentZone = zone[2];
+        }
+        else if (collision.tag == zone[3])
+        {
+            currentZone = zone[3];
+        }
+        else
+        {
+            currentZone = zone[4];
+        }
+    }
+
+    void HitZone()
+    {
+        if (currentZone == zone[0]) //miss
+        {
+            Debug.Log("MISSED");
+        } 
+        else if (currentZone == zone[1]) //bad
+        {
+            Debug.Log("HIT BAD");
+            Destroy(gameObject);
+        } 
+        else if (currentZone == zone[2]) //okay
+        {
+            Debug.Log("HIT OKAY");
+            Destroy(gameObject);
+        } 
+        else if (currentZone == zone[3]) //good
+        {
+            Debug.Log("HIT GOOD");
+            Destroy(gameObject);
+        } 
+        else //perfect
+        {
+            Debug.Log("HIT PERFECT");
+            Destroy(gameObject);
+        }
+    }
+}
