ee89c3d4a3d892a9642499efd29e72003313aad5
MegaDamage <owenspreadbury1@gmail.com>
2026-03-24 14:56:39 -0400

    added tutorials for the minigames

--- Assets/scripts/tutorial/tutorialMinigames.cs
+++ Assets/scripts/tutorial/tutorialMinigames.cs
@@ -1,11 +1,29 @@
+using System.Linq;
 using UnityEngine;
 
 public class tutorialMinigames : MonoBehaviour
 {
+    public GameObject laneChanger;
+    public GameObject Flappybird;
+    public GameObject Tycoon;
+
+    public tutorialManager tutorialManager;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (GameManager.currentday != 0)
+        {
+            if (!GameManager.seenTemple && laneChanger.activeInHierarchy)
+                tutorialManager.playTutorial("Lane Changer", GameManager.tutorialList["Lane Changer"].Keys.First(), 15f, true);
+            else if (!GameManager.seenBird && Flappybird.activeInHierarchy)
+                tutorialManager.playTutorial("Cave Flyer", GameManager.tutorialList["Cave Flyer"].Keys.First(), 15f, true);
+            else if (!GameManager.seenTycoon && Tycoon.activeInHierarchy)
+                tutorialManager.playTutorial("Tycoon", GameManager.tutorialList["Tycoon"].Keys.First(), 15f, true);
+            //else if (!GameManager.seenTycoon && Tycoon.activeInHierarchy)
+            //    tutorialManager.playTutorial("Tycoon", GameManager.tutorialList["Tycoon"].Keys.First(), 15f, true);
+        }
+
     }
 
     // Update is called once per frame
