4bf3c2dcb9c459862e600ea32124bfe9456cda16
Mac Mealey <148462018+Maccy-0@users.noreply.github.com>
2026-03-24 17:06:19 -0400

    Smaller things

--- Assets/menu/DebugSceneSkipper.cs
+++ Assets/menu/DebugSceneSkipper.cs
@@ -11,7 +11,7 @@ public class DebugSceneSkipper : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            LoadNextScene();
+            //LoadNextScene();
         }
     }
 
